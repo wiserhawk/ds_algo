00ce0ea40e60dc5efddd6473868b95dbb8f04077
mkumar <mkumar@clearwateranalytics.com>
2020-03-11 19:49:44 +0530

    Added new problem

---
+++ algos101/src/main/java/datastructures/matrix/StrokesToPaint.java
@@ -0,0 +1,33 @@
+package datastructures.matrix;
+
+/**
+ *
+Problem Statement
+Alex wants to paint a picture but hates taking the brush off the canvas. In one stroke, 
+Alex can only paint the same colored cells which are joined via some edge.
+
+Given the painting, determine the minimum number of strokes to completely paint the picture.
+
+Take for example, the canvas with height given by h = 3 and width given by w = 5 is to be painted with picture picture=[“aabba”, “aabba”, “aaaca”], 
+the diagram below shows the 4 strokes needed to paint the canvas.
+
+        Strokes
+ Canvas  1   	2  	3 	4
+ aabba   aa  	bb   	a
+ aabba   aa  	bb   	a
+ aaaca   aaa    	c 	a
+ *
+ */
+
+public class StrokesToPaint {
+	
+	public static int strokesRequired(char[][] painting) {
+		
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
