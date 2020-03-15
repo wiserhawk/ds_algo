a877e17d3b7645f22c4ef38cdb41dc066a2e32e6
wiserhawk <mj.sharma009@gmail.com>
2020-03-16 00:31:31 +0530

    added new algo problem

---
+++ algos101/src/main/java/algorithms/common/Encoder.java
@@ -0,0 +1,33 @@
+package algorithms.common;
+
+/**
+ * PROBLEM: Find number of possible encoded string for given numeric format string.
+ * 1 - a
+ * 2 - b
+ * 3 - c
+ * . - .
+ * . - .
+ * 25 - y
+ * 26 - z
+ * 
+ * Input: 12; Output: ab, l
+ * Input: 01; Output: null
+ * 
+ * ASKED : FACEBOOK
+ *
+ */
+
+public class Encoder {
+	
+	public String encode(String num) {
+		
+		// TODO - Write your algo here
+		
+	}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
