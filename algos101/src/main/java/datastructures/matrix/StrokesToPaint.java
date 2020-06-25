7128087172902ecd612709e09ed359f7bdf919cf
wiserhawk <mj.sharma009@gmail.com>
2020-06-25 20:34:04 +0530

    updated StrokesToPaint problem

--- algos101/src/main/java/datastructures/matrix/StrokesToPaint.java
+++ algos101/src/main/java/datastructures/matrix/StrokesToPaint.java
@@ -22,12 +22,51 @@ the diagram below shows the 4 strokes needed to paint the canvas.
 public class StrokesToPaint {
 	
 	public static int strokesRequired(char[][] painting) {
+		int row = painting.length;
+		int col = painting[0].length;
+		boolean[][] visited = new boolean[row][col];
 		
+		int stroks = 0;
+		for (int r = 0; r < row; r++) {
+			for (int c = 0; c < col; c++) {
+				if (visited[r][c] == false) {
+					// Check if Adjacent elements are same, 
+					// if yes, mark them visited to avoid further analysis. 
+					markStrok(r, c, row, col, painting, visited);
+					stroks++;
+				}
+			}
+		}
+		return stroks;
+	}
+	
+
+	// Recursive method that check whether Adjacent elements are same, 
+	// if yes, mark them visited (memoization technique) to avoid further analysis on them.
+	private static void markStrok(int r, int c, int h, int w, char[][] painting, boolean[][] visited) {
+		// Mark element visited.
+		visited[r][c] = true;
+		
+		// Check right adjacent
+		if (c+1 < w) // Check is column index out of range. 
+			if (painting[r][c] == painting[r][c+1]) // right adjacent
+				markStrok(r, c+1, h, w, painting, visited);
+		
+		// Check bottom adjacent
+		if (r+1 < h) // Check is row index out of range.
+			if (painting[r][c] == painting[r+1][c]) // bottom adjacent
+				markStrok(r+1, c, h, w, painting, visited);
 	}
 
 	public static void main(String[] args) {
 		
-
+		char[][] painting = new char[][] {{'a', 'a', 'b', 'b', 'a'},
+								 		  {'a', 'a', 'b', 'b', 'a'}, 
+								          {'a', 'a', 'a', 'c', 'a'}};
+								        
+		int stroks = strokesRequired(painting);
+		
+		System.out.println("Number of Stroks Request to Point = " + stroks);
 	}
 
 }
